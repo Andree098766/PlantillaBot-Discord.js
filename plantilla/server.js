c0d3406fbbfc156d24f2b2adf872ef287b6ac4a0
Andree098766 <90814412+Andree098766@users.noreply.github.com>
2021-09-30 12:07:30 -0600

    Add files via upload

---
+++ plantilla/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+
+server.all('/', (req, res) =>{
+  res.send('Aqui estamos Mr. Coding')
+});
+
+function keepalive(){
+  server.listen(3000, () =>{console.log("Servidor listo!" + Date.now())});
+}
+
+module.exports = keepalive; 
